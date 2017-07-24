e13fcb972cdcc5198b827ec1bb12f05ceecde8d1
joao-neves95 <joao95neves@gmail.com>
2017-07-24 14:46:23 +0100

    even-or-odd.js

---
+++ JavaScript/even-or-odd.js
@@ -0,0 +1,13 @@
+/*
+Finding if a number is even or odd.
+JavaScript.
+*/
+
+function even_or_odd(number) {
+  // When a even number is divided by 2, the remainder is 0.
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+    }
+}
