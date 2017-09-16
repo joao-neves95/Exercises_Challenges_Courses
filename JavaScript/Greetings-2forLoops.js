56948316864d369d2fbb3824ea7263e0cd68f26b
joao-neves95 <29694171+joao-neves95@users.noreply.github.com>
2017-09-16 13:27:16 +0100

    Unnecessary variables in Greetings-2forLoops.js
    - Unnecessary variables. There is no need to store the word.substring() values.

--- JavaScript/Greetings-2forLoops.js
+++ JavaScript/Greetings-2forLoops.js
@@ -30,12 +30,10 @@ Greetings
 var word = "Greetings";
 
 for (var i = word.length; i > 0-1; i--) {
-  print = word.substring(0, i);
-  console.log(print);
+  console.log(word.substring(0, i));
   if (i < 0+1) {
     for (var j = 2; j < word.length+1; j++) {
-      rePrint = word.substring(0, j);
-      console.log(rePrint);
+      console.log(word.substring(0, j));
     }
   }
 }
