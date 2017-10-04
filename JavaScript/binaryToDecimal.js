c6a440d6db0f1ae1d3999bf8a2b1847eee047dbf
shivayl <29694171+joao-neves95@users.noreply.github.com>
2017-10-04 11:06:34 +0100

    binaryToDecimal.js
    - Fix to indentation, spaces and others as of JavaScript Standard Style.

--- JavaScript/binaryToDecimal.js
+++ JavaScript/binaryToDecimal.js
@@ -7,35 +7,33 @@ It converts binary base numbers into decimal base ones.
 
 */
 
-function binaryToDecimal(inputNum) {
-    /*
-       split("") doesn't work with numbers, so to index each number from the binary number into an array,
-       it's imperative to convert the binary to a string, and then .split("") each number in order it can proceed:
-    */
-    inputNum = String(inputNum);
-    var binaryStringArray = inputNum.split("");
+function binaryToDecimal (inputNum) {
+  /*
+    split('') doesn't work with numbers, so to index each number from the binary number into an array,
+    it's imperative to convert the binary to a string, and then .split('') each number in order it can proceed:
+  */
+  inputNum = String(inputNum);
+  var binaryStringArray = inputNum.split('');
 
-    // Convert each string value number, into a number value:
-    var binaryNumArray = [];
-    for (var i = 0; i < binaryStringArray.length; i++) {
-        binaryNumArray.push(parseInt(binaryStringArray[i]));
-    }
+  // Convert each string value number, into a number value:
+  var binaryNumArray = [];
+  for (var i = 0; i < binaryStringArray.length; i++) {
+    binaryNumArray.push(parseInt(binaryStringArray[i]));
+  }
 
-    // Reverse the array:
-    var reversedBinaryNumArray = [];
-    for (var j = binaryNumArray.length - 1; j >= 0; j--) {
-        reversedBinaryNumArray.push(binaryNumArray[j]);
-    }
+  // Reverse the array:
+  var reversedBinaryNumArray = [];
+  for (var j = binaryNumArray.length - 1; j >= 0; j--) {
+    reversedBinaryNumArray.push(binaryNumArray[j]);
+  }
 
-    // Calculate the decimal number:
-    var decimalNumber = 0;
-    for (var k = 0; k < reversedBinaryNumArray.length; k++) {
-        decimalNumber += reversedBinaryNumArray[k] * Math.pow(2, k);
-    }
-    return decimalNumber;
+  // Calculate the decimal number:
+  var decimalNumber = 0;
+  for (var k = 0; k < reversedBinaryNumArray.length; k++) {
+    decimalNumber += reversedBinaryNumArray[k] * Math.pow(2, k);
+  }
+  return decimalNumber;
 }
-
-
 
 // Calling the function:
 console.log(binaryToDecimal(1011));
