8b366248ae4cc0cbf79b3810b26521f707f402c0
shivayl <29694171+joao-neves95@users.noreply.github.com>
2017-10-04 11:05:22 +0100

    decimalToBinary.js
    - Fix to indentation, spaces and others as of JavaScript Standard Style.

--- JavaScript/decimalToBinary.js
+++ JavaScript/decimalToBinary.js
@@ -12,36 +12,35 @@ https://github.com/joao-neves95/Exercises_Challenges/blob/master/JavaScript/bina
 
 */
 
-function decimalToBinary(inputNum) {
-    var rest = "";
-    var lastQuotient = 0;
-    var newQuotient = 0;
-    
-    // First division. It divides the first decimal number by 2 (and it gets the first quotient):
-    lastQuotient = Math.floor(inputNum / 2);
-    
-    // Returns the first rest, and adds it into the "rest" string variable:
-    rest += inputNum - Math.floor((lastQuotient * 2));
-    
-    // Conditional. (If the division of the last quotient by 2 is greater than or equal to 1, continue the operation with a for loop):
-    if (lastQuotient >= 1) {
-        for (var i = 0; lastQuotient >= 1; i++) {
-            newQuotient = Math.floor(lastQuotient / 2);
-            rest += lastQuotient - Math.floor((newQuotient * 2));
-            lastQuotient = newQuotient;
-        }
-        // (If the division of the last quotient by 2 is NOT greater than or equal to 1, reverse the "rest" string, and console.log the output):
-    }
-    function reverseString(string) {
-        var reversedString = "";
-        for (var i = string.length - 1; i >= 0; i--) {
-            reversedString += string[i];
-        }
-        return reversedString;
-    }
-    return reverseString(rest);
-}
+function decimalToBinary (inputNum) {
+  var rest = '';
+  var lastQuotient = 0;
+  var newQuotient = 0;
 
+  // First division. It divides the first decimal number by 2 (and it gets the first quotient):
+  lastQuotient = Math.floor(inputNum / 2);
+
+  // Returns the first rest, and adds it into the "rest" string variable:
+  rest += inputNum - Math.floor((lastQuotient * 2));
+
+  // Conditional. (If the division of the last quotient by 2 is greater than or equal to 1, continue the operation with a for loop):
+  if (lastQuotient >= 1) {
+    for (var i = 0; lastQuotient >= 1; i++) {
+      newQuotient = Math.floor(lastQuotient / 2);
+      rest += lastQuotient - Math.floor((newQuotient * 2));
+      lastQuotient = newQuotient;
+    }
+    // (If the division of the last quotient by 2 is NOT greater than or equal to 1, reverse the "rest" string, and console.log the output):
+  }
+  function reverseString (string) {
+    var reversedString = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;
+  }
+  return reverseString(rest);
+}
 
 // Calling the function:
 decimalToBinary(269);
