7e6767c74a064d20f06ed7fd591cb6b557cf2105
shivayl <joao95neves@gmail.com>
2017-11-04 13:51:26 +0000

    Create findLenLongestWord(str).js
    Return the length of the longest word in the provided sentence.

---
+++ JavaScript/findLenLongestWord(str).js
@@ -0,0 +1,25 @@
+/*
+
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+*/
+
+
+function findLongestWord(str) {
+  let splited = str.split(" ")
+
+  let lenghtsArray = splited.map(function (val) {
+    return val.length
+  })
+  
+  lenghtsArray.sort(function (a, b) {
+    return b - a
+  })
+  
+  // Return the first value of the array (the biggest number)
+  return lenghtsArray[0]
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
