6c483c63e49bcb99c86648546818804d74ea8c9d
shivayl <joao95neves@gmail.com>
2017-11-27 20:21:20 +0000

    Update even-or-odd.js

--- JavaScript/even-or-odd.js
+++ JavaScript/even-or-odd.js
@@ -5,9 +5,8 @@ JavaScript.
 
 function even_or_odd(number) {
   // When a even number is divided by 2, the remainder is 0.
-  if (number % 2 == 0) {
-    return "Even";
-  } else {
-    return "Odd";
-    }
+  if (number % 2 == 0)
+    return "Even"
+  else
+    return "Odd"
 }
