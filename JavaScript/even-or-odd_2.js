ef154f0c1483b5c5d184c7df7de04eb61f1d287f
shivayl <joao95neves@gmail.com>
2017-11-27 20:22:08 +0000

    Update even-or-odd_2.js

--- JavaScript/even-or-odd_2.js
+++ JavaScript/even-or-odd_2.js
@@ -7,9 +7,8 @@ and report that to the screen (e.g. "2 is even").
 */
 
 for (var i = 0; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i + " is even");
-  } else {
+  if (i % 2 === 0)
+    console.log(i + " is even")
+  else
     console.log(i + " is odd")
-  }
 }
