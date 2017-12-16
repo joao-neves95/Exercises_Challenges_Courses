736aa0fd02a8ecce8ac665440f877baa31791947
shivayl <joao95neves@gmail.com>
2017-12-16 21:48:44 +0000

    Created index.js
    - Added the homepage routing;
    - Added the RESTful API routing.

--- Node.js/Introduction-to-NodeJS-edX_Microsoft/Lab2-RESTful_Blog_API/routes/index.js
+++ Node.js/Introduction-to-NodeJS-edX_Microsoft/Lab2-RESTful_Blog_API/routes/index.js
@@ -1,3 +1,12 @@
-/**
- * Created by Utilizador on 15/12/2017.
- */
+const router = require('express').Router()
+const posts = require('./posts')
+const comments = require('./comments')
+
+router.use('/', posts)
+router.use('/', comments)
+
+router.get('/', (req, res) => {
+  res.status(200).send('This is the Homepage.')
+})
+
+module.exports = router
