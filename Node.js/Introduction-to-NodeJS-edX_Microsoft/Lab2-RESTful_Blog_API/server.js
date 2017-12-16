b818adc8b1b805006a20d9de3640c912147f2ff8
shivayl <joao95neves@gmail.com>
2017-12-16 21:50:57 +0000

    Update server.js
    - Added various modules and some middleware;
    - Iniciated routing through index.js

--- Node.js/Introduction-to-NodeJS-edX_Microsoft/Lab2-RESTful_Blog_API/server.js
+++ Node.js/Introduction-to-NodeJS-edX_Microsoft/Lab2-RESTful_Blog_API/server.js
@@ -1,3 +1,26 @@
-/**
- * Created by Utilizador on 15/12/2017.
- */
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const helmet = require('helmet')
+const logger = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const db = require('./database')
+const routes = require('./routes/index')
+const PORT = 3000
+const app = express()
+
+// create a write stream (in append mode) - LOGS:
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+app.use(logger('dev'))
+app.use(logger('combined', {stream: accessLogStream}))
+app.use(helmet())
+app.use(compression())
+app.use(bodyParser.json())
+
+app.use('/', routes)
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+})
