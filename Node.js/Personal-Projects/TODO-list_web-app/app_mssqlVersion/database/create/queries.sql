57cb850fae5a931e061d9fc6f2af8b222f3aedb6
shivayl <joao95neves@gmail.com>
2018-02-17 21:05:29 +0000

    Add TODO-List-Web-App MSSQL Version database schema and queries

---
+++ Node.js/Personal-Projects/TODO-list_web-app/app_mssqlVersion/database/create/queries.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM dbo.Users;
+GO
+
+SELECT *
+FROM dbo.LocalAuth;
+GO
+
+DELETE TOP(10)
+FROM dbo.Users;
+GO
+
+DELETE TOP(10)
+FROM dbo.LocalAuth;
+GO
+
+DROP DATABASE IF EXISTS TodoList;
+GO
