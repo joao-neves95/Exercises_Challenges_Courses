6432ab72b873ee3ecc7c4e75120ee29c213487d7
shivayl <joao95neves@gmail.com>
2018-06-15 17:24:37 +0100

    Create Collections.js

---
+++ JavaScript/Collections.js
@@ -0,0 +1,174 @@
+/*
+
+Class Dictionary(): let dictionary = new Dictionary(uniqueKeys = false)
+
+Type safe Class List(): let list = new List('string' | 'number' | 'int' | 'float' | 'boolean')
+
+*/
+
+class Errors {
+  static get existingKey() { throw new Error('An item with the same key has already been added.'); };
+
+  static get noTypeProvided() { throw new Error('No type provided on Collection instantiation.') };
+
+  static wrongType(type) { throw new Error(`The value is not from the same type as the List<${type}>`); };
+}
+
+class Collection {
+  constructor(uniqueKeys, type) {
+    this.elements = [];
+    this.uniqueKeys = (uniqueKeys || false);
+    
+    if (!type) throw Errors.noTypeProvided;
+    this.type = type;
+  }
+
+
+  get length() {
+    return this.elements.length;
+  };
+
+  /**
+   * Get all elements from the Collection.
+   * @returns {any[]} elements
+   */
+  getAll() {
+    return this.elements;
+  }
+
+  /**
+   * Remove all elements from the Collection.
+   */
+  clear() {
+    this.elements = [];
+  };
+
+  /**
+   * (private)
+   * No type safety. For private class use.
+   * @param {Type} value
+   */
+  push(value) {
+    this.elements.push(value);
+  }
+
+ /**
+   * (private)
+   * No checks. For private class use.
+   * @param {Number} index
+   */
+  splice(index) {
+    this.elements.splice(index, 1);
+  }
+}
+
+class Dictionary extends Collection {
+  /**
+   * Whether the keys should be unique or not.
+   * Optional. It defaults to false.
+   * @param {Boolean} uniqueKeys
+   * @default {false}
+   */
+  constructor(uniqueKeys) {
+    super(uniqueKeys, 'any');
+  };
+
+  add(key, value) {
+    if (this.uniqueKeys && this.findIndexOfKey(key) !== undefined)
+      throw new Error(Errors.existingKey);
+
+    this.push({ [key]: value });
+  };
+
+  remove(key) {
+    const index = this.findIndexOfKey(key);
+    if (!index)
+      return false;
+  
+    this.splice(index);
+  };
+
+  getByIndex(index) {
+    return Object.values(this.elements[index]);
+  };
+
+  getByKey(key) {
+    return this.elements[this.findIndexOfKey(key)][key];
+  };
+
+  findIndexOfKey(key, Callback) {
+    for (let i = 0; i < this.elements.length; i++) {
+      if (Object.keys(this.elements[i])[0] === key) {
+        return i;
+      }
+    }
+    return false;
+  }
+}
+
+// Type safe list.
+class List extends Collection {
+  /**
+   * 
+   * The Type of the list.
+   * ('string' | 'number' | 'int' | 'float' | 'boolean')
+   * @param {String} type
+   */
+  constructor(type) {
+    super(false, type);
+  }
+
+  /**
+   * Add a new item to the List<T>.
+   * @param {Type} value
+   */
+  add(value) {
+    switch (this.type) {
+      case 'int':
+        if (this.isInt(value)) {
+          this.push(value);
+          break;
+        }
+      case 'float':
+        if (this.isFloat(value)) {
+          this.push(value);
+          break;
+        }
+      default:
+        if (typeof value === this.type && value !== 'float' && value !== 'int')
+          this.push(value);
+        else
+          throw Errors.wrongType(this.type);
+    }
+  };
+
+  /**
+   * Remove an new item from the List<T> by index.
+   * @param {Number} index
+   */
+  remove(index) {
+    this.splice(index);
+  };
+
+  /**
+   * (private)
+   * @param {Number} value
+   */
+  isInt(value) {
+    if (typeof value !== 'number')
+      return false;
+
+    return value % 1 === 0;
+  }
+
+  /**
+   * (private)
+   * @param {Number} value
+   */
+  isFloat(value) {
+    if (typeof value !== 'number')
+      return false;
+
+    return value % 1 !== 0;
+  }
+}
