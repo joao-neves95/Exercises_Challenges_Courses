01fd008ddc25ec9f54d1e3ea4a8bdca9409eeb80
shivayl <joao95neves@gmail.com>
2018-10-13 22:49:01 +0100

    Update Collections.js
    - Fix on the Dictionary's .getByKey() method;
    - Added the 'any' type to the List.

--- JavaScript/Collections.js
+++ JavaScript/Collections.js
@@ -7,19 +7,19 @@ Type safe Class List(): let list = new List('string' | 'number' | 'int' | 'float
 */
 
 class Errors {
-  static get existingKey() { throw new Error('An item with the same key has already been added.'); };
+  static get existingKey() { throw new Error( 'An item with the same key has already been added.' ); };
 
-  static get noTypeProvided() { throw new Error('No type provided on Collection instantiation.') };
+  static get noTypeProvided() { throw new Error( 'No type provided on Collection instantiation.' ) };
 
-  static wrongType(type) { throw new Error(`The value is not from the same type as the List<${type}>`); };
+  static wrongType( type ) { throw new Error( `The value is not from the same type as the List<${type}>` ); };
 }
 
 class Collection {
-  constructor(uniqueKeys, type) {
+  constructor( uniqueKeys, type ) {
     this.elements = [];
-    this.uniqueKeys = (uniqueKeys || false);
+    this.uniqueKeys = ( uniqueKeys || false );
 
-    if (!type) throw Errors.noTypeProvided;
+    if ( !type ) throw Errors.noTypeProvided;
     this.type = type;
   }
 
@@ -37,6 +37,14 @@ class Collection {
   }
 
   /**
+   * 
+   * @param { number } index
+   */
+  get( index ) {
+    return this.elements[index];
+  }
+
+  /**
    * Remove all elements from the Collection.
    */
   clear() {
@@ -48,8 +56,8 @@ class Collection {
    * No type safety. For private class use.
    * @param {Type} value
    */
-  push(value) {
-    this.elements.push(value);
+  push( value ) {
+    this.elements.push( value );
   }
 
   /**
@@ -57,45 +65,45 @@ class Collection {
     * No checks. For private class use.
     * @param {Number} index
     */
-  splice(index) {
-    this.elements.splice(index, 1);
+  splice( index ) {
+    this.elements.splice( index, 1 );
   }
 }
 
 class Dictionary extends Collection {
   /**
-   * Whether the keys should be unique or not.
-   * Optional. It defaults to false.
-   * @param {Boolean} uniqueKeys
+   * Dictionary of key-value pairs.
+   * @param {Boolean} uniqueKeys Whether the keys should be unique or not.
+   * Optional. It defaults to false
    * @default {false}
    */
-  constructor(uniqueKeys) {
-    super(uniqueKeys, 'any');
-  };
+  constructor( uniqueKeys ) {
+    super( uniqueKeys, 'any' );
+  }
 
   getAllValues() {
     let allValues = [];
 
-    for (let i = 0; i < this.elements.length; ++i) {
-      allValues.push(Object.values(this.elements[i])[0]);
+    for ( let i = 0; i < this.elements.length; ++i ) {
+      allValues.push( Object.values( this.elements[i] )[0] );
     }
 
     return allValues;
   }
 
-  add(key, value) {
-    if (this.uniqueKeys && this.findIndexOfKey(key) !== false)
-      throw new Error(Errors.existingKey);
+  add( key, value ) {
+    if ( this.uniqueKeys && this.findIndexOfKey( key ) !== false )
+      throw new Error( Errors.existingKey );
 
-    this.push({ [key]: value });
+    this.push( { [key]: value } );
   };
 
-  remove(key) {
-    const index = this.findIndexOfKey(key);
-    if (!index)
+  remove( key ) {
+    const index = this.findIndexOfKey( key );
+    if ( index === false )
       return false;
 
-    this.splice(index);
+    this.splice( index );
   };
 
   /**
@@ -103,7 +111,7 @@ class Dictionary extends Collection {
    * @param {number} index
    * @return {any[]}
    */
-  getByIndex(index) {
+  getByIndex( index ) {
     return Object.values( this.elements[index] )[0];
   };
 
@@ -112,24 +120,35 @@ class Dictionary extends Collection {
    * @param {number} index
    * @return {any}
    */
-  getKeyByIndex(index) {
+  getKeyByIndex( index ) {
     return Object.keys( this.elements[index] )[0];
   }
 
-  getByKey(key) {
+  /**
+   * Returns the value by key or <false> if not found.
+   * @param { any } key
+   * @returns { any | false }
+   */
+  getByKey( key ) {
     try {
-      return this.elements[this.findIndexOfKey(key)][key];
-    } catch (e) {
-      console.error(e);
-    }
-  };
+      const keyIdx = this.findIndexOfKey( key );
 
-  findIndexOfKey(key, Callback) {
-    for (let i = 0; i < this.elements.length; i++) {
-      if (Object.keys(this.elements[i])[0] === key) {
-        return i;
-      }
+      if ( keyIdx === false )
+        return false;
+
+      return this.elements[keyIdx][key];
+
+    } catch ( e ) {
+      console.error( e );
     }
+  }
+
+  findIndexOfKey( key ) {
+    for ( let i = 0; i < this.elements.length; i++ ) {
+      if ( Object.keys( this.elements[i] )[0] === key )
+        return i;
+    }
+
     return false;
   }
 }
@@ -139,51 +158,54 @@ class List extends Collection {
   /**
    * 
    * The Type of the list.
-   * ('string' | 'number' | 'int' | 'float' | 'boolean')
+   * ('string' | 'number' | 'int' | 'float' | 'boolean' | 'any')
    * @param {String} type
    */
-  constructor(type) {
-    super(false, type);
+  constructor( type ) {
+    super( false, type );
   }
 
   /**
    * Add a new item to the List<T>.
    * @param {Type} value
    */
-  add(value) {
-    switch (this.type) {
+  add( value ) {
+    switch ( this.type ) {
+      case 'any':
+        this.push( value );
+        break;
       case 'int':
-        if (this.isInt(value)) {
-          this.push(value);
+        if ( this.isInt( value ) ) {
+          this.push( value );
           break;
         }
       case 'float':
-        if (this.isFloat(value)) {
-          this.push(value);
+        if ( this.isFloat( value ) ) {
+          this.push( value );
           break;
         }
       default:
-        if (typeof value === this.type && value !== 'float' && value !== 'int')
-          this.push(value);
+        if ( typeof value === this.type && value !== 'float' && value !== 'int' )
+          this.push( value );
         else
-          throw Errors.wrongType(this.type);
+          throw Errors.wrongType( this.type );
     }
-  };
+  }
 
   /**
    * Remove an new item from the List<T> by index.
    * @param {Number} index
    */
-  remove(index) {
-    this.splice(index);
+  remove( index ) {
+    this.splice( index );
   };
 
   /**
    * (private)
    * @param {Number} value
    */
-  isInt(value) {
-    if (typeof value !== 'number')
+  isInt( value ) {
+    if ( typeof value !== 'number' )
       return false;
 
     return value % 1 === 0;
@@ -193,8 +215,8 @@ class List extends Collection {
    * (private)
    * @param {Number} value
    */
-  isFloat(value) {
-    if (typeof value !== 'number')
+  isFloat( value ) {
+    if ( typeof value !== 'number' )
       return false;
 
     return value % 1 !== 0;
