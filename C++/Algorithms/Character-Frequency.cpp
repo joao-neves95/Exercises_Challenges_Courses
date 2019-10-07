fcaa81c42a579e4bc0bc5ca0126cd9c6475edd88
shivayl <joao95neves@gmail.com>
2019-10-07 22:45:27 +0100

    Fix comment line length

--- C++/Algorithms/Character-Frequency.cpp
+++ C++/Algorithms/Character-Frequency.cpp
@@ -5,9 +5,13 @@
 Character Frequency
 -------------------
 
-Write a function that takes a piece of text in the form of a string and returns the letter frequency count for the text. This count excludes numbers, spaces and all punctuation marks. Upper and lower case versions of a character are equivalent and the result should all be in lowercase.
+Write a function that takes a piece of text in the form of a string and returns the letter frequency count for the text.
+This count excludes numbers, spaces and all punctuation marks. Upper and lower case versions of a character are 
+equivalent and the result should all be in lowercase.
 
-The function should return a list of tuples (in Python and Haskell) or arrays (in other languages) sorted by the most frequent letters first. The Rust implementation should return an ordered BTreeMap. Letters with the same frequency are ordered alphabetically. For example:
+The function should return a list of tuples (in Python and Haskell) or arrays (in other languages) sorted by 
+the most frequent letters first. The Rust implementation should return an ordered BTreeMap. Letters with the 
+same frequency are ordered alphabetically. For example:
 
   letter_frequency('aaAabb dddDD hhcc')
 
