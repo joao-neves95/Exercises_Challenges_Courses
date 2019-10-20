40e4287bc42296671e6f73bb55f7a7db2a3b782e
João Neves <joao95neves@gmail.com>
2019-10-20 15:01:41 +0100

    Revert "Remove old files from academic-work"
    This reverts commit 344bac03b32d1fb75662234fe7c3a338fa0ae9d4.

---
+++ Unity/Project1/Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+    public ParticleSystem thisParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+        thisParticleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (thisParticleSystem.isStopped)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Unity/Project1/Assets/Scripts/NinjaStarController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaStarController : MonoBehaviour {
+    private PlayerController player;
+    private Rigidbody2D rb;
+    public GameObject enemyDeathEffect;
+    public GameObject impactEffect;
+
+    public float SPEED;
+    public float ROTATION_SPEED;
+    public int KILL_POINTS;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerController>();
+
+        if (player.transform.localScale.x < 0)
+        {
+            SPEED = -SPEED;
+            ROTATION_SPEED = -ROTATION_SPEED;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(SPEED, rb.velocity.y);
+        rb.angularVelocity = ROTATION_SPEED;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            ScoreManager.AddPoints(KILL_POINTS);
+            Instantiate(enemyDeathEffect, collision.transform.position, collision.transform.rotation);
+            Destroy(collision.gameObject);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
