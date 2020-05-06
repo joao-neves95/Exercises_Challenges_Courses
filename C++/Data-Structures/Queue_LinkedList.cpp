d1eb7238c4f2015299335423a2cbb00335ab2ff6
João Neves <joao95neves@gmail.com>
2020-05-06 22:47:34 +0100

    [ C++/DataStructures/singleLinkedList/ ]: Changed the folder name

--- C++/Data-Structures/Queue_LinkedList.cpp
+++ C++/Data-Structures/Queue_LinkedList.cpp
@@ -1,6 +1,6 @@
 /*
 
-By: Jo„o Neves (SHIVAYL)
+By: Jo√£o Neves (SHIVAYL)
 
 */
 #include <iostream>
@@ -13,7 +13,7 @@ class LinkedListNode {
     public:
         T value;
         LinkedListNode<T> *next;
-        
+
         LinkedListNode(T value) {
             this->value = value;
             this->next = NULL;
@@ -27,78 +27,78 @@ class LinkedList {
             this->head = NULL;
             this->tail = NULL;
         }
-        
+
         LinkedListNode<T> *head;
         LinkedListNode<T> *tail;
 };
 
 template<typename T>
 class QueueLinkedList {
-    
+
     private:
         int count;
         LinkedList<T> *items;
-    
+
     public:
         QueueLinkedList() {
             this->items = new LinkedList<T>();
             this->count = 0;
         }
-        
+
         size_t size() {
             return this->count;
         }
-        
+
         void enqueue(T item) {
             LinkedListNode<T> *newItem = new LinkedListNode<T>(item);
-            
+
             if (this->count == 0) {
                 this->items->head = newItem;
-            
+
             } else {
                 this->items->tail->next = newItem;
             }
-            
+
             this->items->tail = newItem;
             ++this->count;
         }
-        
+
         T dequeue() {
             LinkedListNode<T> *headNode = this->items->head;
-            
+
             this->items->head = this->items->head->next;
-            
+
             T headValue = headNode->value;
             delete headNode;
             headNode = NULL;
             --this->count;
-            return headValue;            
+            return headValue;
         }
-        
+
         void dispose() {
             LinkedListNode<T> *currNode = this->items->head;
             LinkedListNode<T> *next;
-            
+
             while(currNode != NULL) {
                 next = currNode->next;
                 delete currNode;
                 currNode = next;
             }
-            
+
             delete next;
         }
-        
+
         void printAllItems() {
             LinkedListNode<T> *currNode = this->items->head;
-            
+
             while(currNode != NULL) {
                 cout << currNode->value << " ";
                 currNode = currNode->next;
             }
-            
+
             cout << endl;
         }
-    
+
 };
 
 int main(int argc, char *argv[]) {
@@ -109,23 +109,21 @@ int main(int argc, char *argv[]) {
     queueOfStrings->enqueue("2");
     queueOfStrings->enqueue("3");
     queueOfStrings->enqueue("4");
-    
+
     cout << "Print all queued elements" << endl;
     queueOfStrings->printAllItems();
     cout << "Queue size: " << queueOfStrings->size() << endl;
-    
+
     cout << "Dequeue one element" << endl;
     queueOfStrings->dequeue();
-    
+
     cout << "Print all queued elements" << endl;
     queueOfStrings->printAllItems();
     cout << "Queue size: " << queueOfStrings->size() << endl;
-    
+
     queueOfStrings->dispose();
 
 
 	system("pause");
     return 0;
 }
-
-
--- C++/Data-Structures/w3resource_linked-list_exercises/README.md
+++
@@ -1,1 +0,0 @@
-# [w3resource.com](https://www.w3resource.com) - [C/C++ Programming Exercises, Practice, Solution : Linked List](https://www.w3resource.com/c-programming-exercises/linked_list/index.php)
