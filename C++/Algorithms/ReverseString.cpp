0e7d850df5af9d4ab978dce95d085315dd553597
João Neves <joao95neves@gmail.com>
2021-03-17 15:18:54 +0000

    [C++/Algos/ReverseString]: Added vanilla reverse swap without temp var

--- C++/Algorithms/ReverseString.cpp
+++ C++/Algorithms/ReverseString.cpp
@@ -17,24 +17,24 @@ By: SHIVAYL (João Neves)
 #include <cstdio>
 #include <string>
 #include <algorithm>
-using namespace std ; 
+using namespace std ;
 
 class ReverseString {
 	private:
 		ReverseString() {}
-	
+
 	public:
 		static string withoutSwap(string str) {
 			string result = "";
-  
+
 		    int i;
 		    for (i = str.size() - 1; i >= 0; --i) {
 		      result += str[i];
 		    }
-		  
+
 		    return result;
 		}
-		
+
 		static string withSwap(string str) {
 			  char temp;
 			  int begin;
@@ -45,10 +45,27 @@ class ReverseString {
 			    str[begin] = str[end];
 			    str[end] = temp;
 			  }
-  
+
             return str;
 		}
-		
+
+		static string withSwapWithoutTempVar(string s) {
+			int iLeft;
+        	int iRight;
+
+        	for (iLeft = 0, iRight = s.size() - 1; iLeft < iRight; ++iLeft, --iRight) {
+        	    // We add left and right, on the left side.
+        	    s[iLeft] = s[iLeft] + s[iRight];
+        	    // Get right by subtracting the right from the sum of both.
+        	    s[iRight] = s[iLeft] - s[iRight];
+        	    // Get left by subtracting the original left, now right, from
+        	    // the sum of both, still on the left.
+        	    s[iLeft] = s[iLeft] - s[iRight];
+        	}
+
+			return s;
+		}
+
 		static string withSTL(string str) {
 			std::reverse(std::begin(str), std::end(str));
             return str;
@@ -58,21 +75,22 @@ class ReverseString {
 int main() {
 	// Yes, this is a real word.
 	string stringToReverse1 = "Pneumonoultramicroscopicsilicovolcanoconiosis";
-	
+
 	cout << "String to reverse 1 (yes, this is a real word):\n" << stringToReverse1 << endl << endl;
 	cout << "With the STL:\n" << ReverseString::withSTL(stringToReverse1) << endl << endl;
 	cout << "Without the STL, with swap logic:\n" << ReverseString::withSwap(stringToReverse1) << endl << endl;
+	cout << "Without the STL, with swap logic, without temp var:\n" << ReverseString::withSwapWithoutTempVar(stringToReverse1) << endl << endl;
 	cout << "Without the STL, without swap logic:\n" << ReverseString::withoutSwap(stringToReverse1) << endl << endl << endl;
-	
-	
+
+
 	string stringToReverse2 = "world";
-	
+
 	cout << "String to reverse 2:\n" << stringToReverse2 << endl << endl;
 	cout << "With the STL:\n" << ReverseString::withSTL(stringToReverse2) << endl << endl;
 	cout << "Without the STL, with swap logic:\n" << ReverseString::withSwap(stringToReverse2) << endl << endl;
+	cout << "Without the STL, with swap logic, without temp var:\n" << ReverseString::withSwapWithoutTempVar(stringToReverse2) << endl << endl;
 	cout << "Without the STL, without swap logic:\n" << ReverseString::withoutSwap(stringToReverse2) << endl << endl;
-	
+
 	getchar();
 	return 0;
 }
-
