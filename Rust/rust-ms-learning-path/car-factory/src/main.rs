044f2c3d78ad71dfba39c870610367dd3849e406
João Neves <joao95neves@gmail.com>
2022-03-01 22:38:58 +0000

    [Rust/]: Added the (rust-ms-learning-path/) Microsoft's Learn Rust Learning Path, with an HelloWorld and the WIP CarFactory to save progress

---
+++ Rust/rust-ms-learning-path/car-factory/src/main.rs
@@ -0,0 +1,106 @@
+#[derive(Clone, Debug)]
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    age: (Age, u32),
+}
+
+// impl Car {
+//     fn add_mileage(&mut self, mileage: u32) {
+//         self.mileage += mileage;
+//     }
+// }
+
+#[derive(Clone, PartialEq, Debug)]
+enum Age {
+    New,
+    Used,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_quality(miles: u32) -> (Age, u32) {
+    (if miles == 0 { Age::New } else { Age::Used }, miles)
+}
+
+fn car_factory(order_id: u32, miles: u32, print_details: Option<bool>) -> Car {
+    let colors = [
+        "Black".to_string(),
+        "White".to_string(),
+        "Grey".to_string(),
+        "Silver".to_string(),
+        "Gold".to_string(),
+    ];
+
+    let mut color_idx = order_id as usize;
+    while color_idx > colors.len() - 1 {
+        color_idx = color_idx - (colors.len() - 1);
+    }
+
+    let transmission: Transmission;
+    let mut convertible = false;
+    if order_id % 2 == 0 {
+        transmission = Transmission::Manual;
+    } else if order_id % 3 == 0 {
+        transmission = Transmission::Automatic;
+        convertible = true;
+    } else {
+        transmission = Transmission::SemiAuto;
+    }
+
+    let car = Car {
+        color: colors[color_idx].to_string(),
+        transmission: transmission,
+        convertible: convertible,
+        age: car_quality(miles),
+    };
+
+    if print_details.unwrap_or(false) {
+        print_convertible_car_factory_details(&car);
+    }
+
+    return car;
+}
+
+fn print_convertible_car_factory_details(car: &Car) {
+    if !(car.convertible) {
+        return;
+    }
+
+    println!(
+        "This is a used non-convertible car! Here are some details: {:?}, color {}, hard top, with {} miles\n",
+        car.transmission, car.color, car.age.1
+    );
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    println!("Let's create some cars.");
+
+    let cars_to_create_num = 10;
+
+    let mut cars: Vec<Car> = Vec::new();
+
+    let mut i: u32 = 0;
+    while i < cars_to_create_num {
+        let is_even: bool = i % 2 == 0;
+
+        let new_car = car_factory(i, if is_even { 0 } else { i * 1000 }, Some(true));
+
+        println!("{:#?}", new_car);
+        cars.push(new_car);
+
+        i = i + 1;
+    }
+
+    // println!("Let's increase mileage.");
+    // new_car.add_mileage(420);
+    // println!("{:#?}", new_car);
+}
