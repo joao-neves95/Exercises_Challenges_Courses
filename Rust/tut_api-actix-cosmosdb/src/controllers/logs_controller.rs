b58789b6c76bf8fd2be9f4c71a4a1af9fdabc284
João Neves <joao95neves@gmail.com>
2022-03-24 16:48:25 +0000

    [Rust/tut_api-actix-cosmosdb/]: logs_controller - implemented `.get_logs()`

--- Rust/tut_api-actix-cosmosdb/src/controllers/logs_controller.rs
+++ Rust/tut_api-actix-cosmosdb/src/controllers/logs_controller.rs
@@ -1,4 +1,16 @@
-use actix_web::{web, Responder};
+use actix_web::{
+    error::ErrorInternalServerError,
+    web::{self, Json},
+    Responder,
+};
+use anyhow::Context;
+use bson::{doc, Document};
+use futures::stream::StreamExt;
+use mongodb::{options::FindOptions, Client};
+use std::sync::Mutex;
+
+const MONGO_DB: &'static str = "logs_1";
+const MONGO_DB_COLL_LOGS: &'static str = "logs";
 
 pub fn service_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -8,8 +20,32 @@ pub fn service_config(cfg: &mut web::ServiceConfig) {
     );
 }
 
-async fn get_logs() -> impl Responder {
-    String::from("NOT YET IMPLEMENTED.")
+async fn get_logs(data: web::Data<Mutex<Client>>) -> impl Responder {
+    let logs_collection = data
+        .lock()
+        .unwrap()
+        .database(MONGO_DB)
+        .collection(MONGO_DB_COLL_LOGS);
+
+    let filter = doc! {};
+    let find_options = FindOptions::builder().sort(doc! { "_id": -1i32 }).build();
+    let mut cursor = logs_collection
+        .find(filter, find_options)
+        .await
+        .with_context(|| format!("Error while connecting to MongoDB"))
+        .unwrap();
+
+    let mut results: Vec<Document> = Vec::new();
+
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(document) => results.push(document),
+            _ => return Err(ErrorInternalServerError(format!("ERROR"))),
+            // _ => return Ok::<Json<Vec<Document>>, Error>(Json(Vec::new())),
+        }
+    }
+
+    Ok(Json(results))
 }
 
 async fn post_log() -> impl Responder {
