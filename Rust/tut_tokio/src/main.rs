12734d121e21ee478dd625d79f8d9e2e8f866203
João Neves <joao95neves@gmail.com>
2022-05-31 18:55:01 +0100

    [Rust/tut_tokio/]: Set up GET/POST handling

--- Rust/tut_tokio/src/main.rs
+++ Rust/tut_tokio/src/main.rs
@@ -4,14 +4,11 @@ use std::{
     str::FromStr,
 };
 
-use tokio::{
-    io::AsyncReadExt,
-    net::{TcpListener, TcpStream},
-};
+use tokio::{io::AsyncReadExt, net::TcpListener};
 
 use strum::{Display, EnumString};
 
-use request_handlers::handle_get_balance;
+use request_handlers::{handle_get_balance, handle_post_balance};
 mod request_handlers;
 
 #[derive(Debug, PartialEq, EnumString, Display)]
@@ -27,6 +24,7 @@ struct InternalError {
 }
 
 impl InternalError {
+    #[allow(dead_code)]
     fn new(message: &str) -> InternalError {
         InternalError {
             message: message.to_string(),
@@ -57,7 +55,7 @@ impl Display for InternalError {
 impl Error for InternalError {}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ! {
     let task = tokio::spawn(async {
         println!("Hello, world!");
     });
@@ -70,30 +68,32 @@ async fn main() {
 
     loop {
         match tcp_listener.accept().await {
-            Ok((mut stream, addy)) => {
-                let http_method = match get_http_method_type(&mut stream).await {
-                    Ok(method) => method.to_string(),
+            Ok((mut stream, addy)) => tokio::spawn(async move {
+                let mut request_buffer: [u8; 16] = [0; 16];
+                stream.read(&mut request_buffer).await.unwrap();
+
+                let http_method = match get_http_method_type(request_buffer).await {
+                    Ok(method) => method,
                     Err(e) => {
                         println!("Error while handling the HTTP call: {e:?}");
-
-                        break;
+                        return;
                     }
                 };
 
-                println!("New {http_method} request from: {addy}");
+                println!("New {} request from: {addy}", http_method.to_string());
 
-                tokio::spawn(async { handle_get_balance(stream).await });
-            }
-            Err(e) => println!("Error while getting request: {e:?}"),
-        }
+                match http_method {
+                    HttpMethodType::GET => handle_get_balance(stream).await,
+                    HttpMethodType::POST => handle_post_balance(stream, &mut request_buffer).await,
+                };
+            }),
+            Err(e) => tokio::spawn(async move { println!("Error while getting request: {e:?}") }),
+        };
     }
 }
 
-async fn get_http_method_type(stream: &mut TcpStream) -> Result<HttpMethodType, InternalError> {
-    let mut buffer: [u8; 16] = [0; 16];
-    stream.read(&mut buffer).await.unwrap();
-
-    return match std::str::from_utf8(&buffer[0..4]) {
+async fn get_http_method_type(request_buffer: [u8; 16]) -> Result<HttpMethodType, InternalError> {
+    return match std::str::from_utf8(&request_buffer[0..4]) {
         Ok(method) => match HttpMethodType::from_str(method.trim_end().to_uppercase().as_str()) {
             Ok(parsed_method) => Ok(parsed_method),
             Err(e) => Err(InternalError::new_inner(
--- Rust/tut_tokio/src/request_handlers.rs
+++ Rust/tut_tokio/src/request_handlers.rs
@@ -1,8 +1,24 @@
 use tokio::{io::AsyncWriteExt, net::TcpStream};
 
-pub async fn handle_get_balance(mut stream: TcpStream) {
-    let content = "{\"balance\": 0.00}";
+pub async fn handle_get_balance(stream: TcpStream) {
+    build_response(stream, format!("{{\"balance\": {}}}", 0.00)).await;
+}
 
+pub async fn handle_post_balance(stream: TcpStream, request_buffer: &mut [u8; 16]) {
+    // TODO: Error handling.
+
+    // Take the content after 'POST /' unntil whitespace.
+    let input: String = request_buffer[6..16]
+        .iter()
+        .take_while(|c| !(**c as char).is_whitespace())
+        .map(|c| *c as char)
+        .collect();
+
+    let new_balance = input.parse::<f32>().unwrap();
+    build_response(stream, format!("{{\"balance\": {}}}", new_balance)).await;
+}
+
+async fn build_response(mut stream: TcpStream, content: String) {
     let response = format!(
         "HTTP/1.1 200 OK\nContent-Type: application/json\r\nContent-Length: {}\n\n{}",
         content.len(),
