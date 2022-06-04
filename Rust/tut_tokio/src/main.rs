78ed5d10b20bda378b3593f3dad9ca5ea2398bf3
João Neves <joao95neves@gmail.com>
2022-06-04 20:26:23 +0100

    [Rust/tut_tokio/]: Multiple refactorings

--- Rust/tut_tokio/src/main.rs
+++ Rust/tut_tokio/src/main.rs
@@ -4,7 +4,10 @@ use std::{
     str::FromStr,
 };
 
-use tokio::{io::AsyncReadExt, net::TcpListener};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 use strum::{Display, EnumString};
 
@@ -15,6 +18,12 @@ mod request_handlers;
 enum HttpMethodType {
     GET,
     POST,
+}
+
+pub struct Response {
+    status_code: u16,
+    status: String,
+    content: String,
 }
 
 #[derive(Debug)]
@@ -55,49 +64,70 @@ impl Display for InternalError {
 impl Error for InternalError {}
 
 #[tokio::main]
-async fn main() -> ! {
+async fn main() {
     let task = tokio::spawn(async {
         println!("Hello, world!");
     });
 
     task.await.unwrap();
 
-    let server_addy = "127.0.0.1:5151";
-    let tcp_listener = TcpListener::bind(server_addy).await.unwrap();
-    println!("Server listening on {server_addy}...");
+    const SERVER_ADDY: &str = "127.0.0.1:5151";
+    let tcp_listener = TcpListener::bind(SERVER_ADDY).await.unwrap();
+    println!("Server listening on {SERVER_ADDY}...");
 
     loop {
         match tcp_listener.accept().await {
-            Ok((mut stream, addy)) => tokio::spawn(async move {
-                let mut request_buffer: [u8; 16] = [0; 16];
-                stream.read(&mut request_buffer).await.unwrap();
-
-                let http_method = match get_http_method_type(request_buffer).await {
-                    Ok(method) => method,
-                    Err(e) => {
-                        println!("Error while handling the HTTP call: {e:?}");
-                        return;
-                    }
-                };
-
-                println!("New {} request from: {addy}", http_method.to_string());
-
-                match http_method {
-                    HttpMethodType::GET => handle_get_balance(stream).await,
-                    HttpMethodType::POST => handle_post_balance(stream, &mut request_buffer).await,
-                };
+            Ok((stream, addy)) => tokio::spawn(async move {
+                handle_connection(stream, addy.to_string()).await;
             }),
             Err(e) => tokio::spawn(async move { println!("Error while getting request: {e:?}") }),
         };
     }
 }
 
-async fn get_http_method_type(request_buffer: [u8; 16]) -> Result<HttpMethodType, InternalError> {
+async fn handle_connection(mut stream: TcpStream, client_address: String) {
+    let mut request_buffer: [u8; 32] = [0; 32];
+    stream.read(&mut request_buffer).await.unwrap();
+
+    let http_method = match get_http_method_type(request_buffer).await {
+        Ok(method) => method,
+        Err(e) => {
+            println!("Error while handling the HTTP call: {e:?}");
+            return;
+        }
+    };
+
+    println!(
+        "New {} request from: {}",
+        http_method.to_string(),
+        client_address
+    );
+
+    let res = match http_method {
+        HttpMethodType::GET => handle_get_balance().await,
+        HttpMethodType::POST => handle_post_balance(&mut request_buffer).await,
+    };
+
+    let http_response = format!(
+        "HTTP/1.1 {} {}\nContent-Type: application/json\nContent-Length: {}\n\n{}",
+        res.status_code,
+        res.status,
+        res.content.len(),
+        res.content
+    );
+
+    println!("Response: {http_response:?}");
+
+    stream.write(http_response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
+}
+
+async fn get_http_method_type(request_buffer: [u8; 32]) -> Result<HttpMethodType, InternalError> {
     return match std::str::from_utf8(&request_buffer[0..4]) {
         Ok(method) => match HttpMethodType::from_str(method.trim_end().to_uppercase().as_str()) {
             Ok(parsed_method) => Ok(parsed_method),
             Err(e) => Err(InternalError::new_inner(
-                "Invalid HTTP method type format",
+                "Invalid HTTP method type",
                 e.to_string().as_str(),
             )),
         },
--- Rust/tut_tokio/src/request_handlers.rs
+++ Rust/tut_tokio/src/request_handlers.rs
@@ -1,30 +1,43 @@
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use crate::Response;
 
-pub async fn handle_get_balance(stream: TcpStream) {
-    build_response(stream, format!("{{\"balance\": {}}}", 0.00)).await;
+pub async fn handle_get_balance() -> Response {
+    build_response(200, format!("{{\"balance\": {}}}", 0.00)).await
 }
 
-pub async fn handle_post_balance(stream: TcpStream, request_buffer: &mut [u8; 16]) {
-    // TODO: Error handling.
-
+pub async fn handle_post_balance(request_buffer: &mut [u8; 32]) -> Response {
     // Take the content after 'POST /' unntil whitespace.
-    let input: String = request_buffer[6..16]
+    let input: String = request_buffer[6..32]
         .iter()
         .take_while(|c| !(**c as char).is_whitespace())
         .map(|c| *c as char)
         .collect();
 
+    if input.is_empty() {
+        return build_response(
+            400,
+            "Error: The new balance amount parameter is required.".to_string(),
+        )
+        .await;
+    }
+
     let new_balance = input.parse::<f32>().unwrap();
-    build_response(stream, format!("{{\"balance\": {}}}", new_balance)).await;
+    println!("New balance: {}", new_balance);
+
+    build_response(200, format!("{{\"balance\": {}}}", new_balance)).await
 }
 
-async fn build_response(mut stream: TcpStream, content: String) {
-    let response = format!(
-        "HTTP/1.1 200 OK\nContent-Type: application/json\r\nContent-Length: {}\n\n{}",
-        content.len(),
-        content
-    );
+async fn build_response(status_code: u16, content: String) -> Response {
+    let status = match status_code {
+        200 => "OK",
+        400 => "Bad Request",
 
-    stream.write(response.as_bytes()).await.unwrap();
-    stream.flush().await.unwrap();
+        _ => "Not Implemented",
+    }
+    .to_string();
+
+    Response {
+        status_code,
+        status,
+        content,
+    }
 }
