d4a25a25a811ace0e69fbb586c7dce40c49485eb
João Neves <joao95neves@gmail.com>
2022-06-14 00:14:23 +0100

    [Rust/l/]: Print dir

--- Rust/l/src/main.rs
+++ Rust/l/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Error handling.
+    let all_entries = fs::read_dir(env::current_dir().unwrap())
+        .unwrap()
+        .map(|entry_res| match entry_res.unwrap().path().file_name() {
+            Some(name) => name.to_os_string().into_string().unwrap(),
+            None => "..".to_owned(),
+        })
+        .collect::<Vec<String>>();
+
+    for entry in all_entries {
+        println!("{}", entry);
+    }
 }
