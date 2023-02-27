deaaabf542c5a1d7c50ae1baba676f4ac2ae650e
João Neves <joao95neves@gmail.com>
2023-02-27 18:12:10 +0000

    [CSharp/]: Added AlgosAndDataStructs

---
+++ CSharp/AlgosAndDataStructs/AlgosAndDataStructs.Algorithms/Linear.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+namespace AlgosAndDataStructs.Algorithms
+{
+    public static class Linear
+    {
+        public static int LinearFind<T>(this IEnumerable<T> @list, T itemToFind)
+        {
+            if (@list is null)
+            {
+                throw new ArgumentNullException(nameof(@list));
+            }
+
+            for (int i = 0; i < @list.Count(); ++i)
+            {
+                T? element = @list.ElementAtOrDefault(i);
+
+                if (element is not null && element.Equals(itemToFind))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
