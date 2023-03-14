7b44850dc378ef1da1795d8a8597b903962840bb
João Neves <joao95neves@gmail.com>
2023-03-14 16:33:02 +0000

    [CSharp\AlgosAndDataStructs\Search\]: added ExistsInBothCollections

---
+++ CSharp/AlgosAndDataStructs/AlgosAndDataStructs.Algorithms/Search/ExistsInBothCollections.cs
@@ -0,0 +1,89 @@
+
+namespace AlgosAndDataStructs.Algorithms.Search
+{
+    public static class ExistsInBothCollections
+    {
+        /// <summary>
+        /// Speed: O(a * b) <br />
+        /// Memory: O(1)
+        ///
+        /// </summary>
+        public static bool BruteForce<T>(IEnumerable<T> array1, IEnumerable<T> array2)
+        {
+            if (!IsInputValid(array1, array2))
+            {
+                return false;
+            }
+
+            // O(a).
+            for (int i = 0; i < array1.Count(); ++i)
+            {
+                // O(b).
+                for (int j = 0; j < array2.Count(); ++j)
+                {
+                    if (array1.ElementAt(i)?.Equals(array2.ElementAt(j)) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Speed: O(a + b) <br />
+        /// Memory: O(MAX(a, b))
+        ///
+        /// </summary>
+        public static bool Optimized<T>(IEnumerable<T> array1, IEnumerable<T> array2)
+        {
+            if (!IsInputValid(array1, array2))
+            {
+                return false;
+            }
+
+            // Don't do this logic.
+            // It adds complexity and this type of optimizations should only be done when needed.
+            bool array1IsBigger = array1.Count() >= array2.Count();
+
+            HashSet<T> set = ArrayToHashSet(array1IsBigger ? array1 : array2)!;
+
+            // O(b) + O(1) = O(b).
+            return (array1IsBigger ? array2 : array1)
+                .Any(item => set.Contains(item));
+        }
+
+        private static bool IsInputValid<T>(IEnumerable<T> array1, IEnumerable<T> array2)
+        {
+            return array1 is not null && array2 is not null && array1.Any() && array2.Any();
+        }
+
+        /// <summary>
+        /// O(n)
+        ///
+        /// </summary>
+        private static HashSet<T>? ArrayToHashSet<T>(IEnumerable<T> array)
+        {
+            if (array?.Any() != true)
+            {
+                return default!;
+            }
+
+            HashSet<T> set = new(array.Count());
+
+            // O(n).
+            foreach (T item in array)
+            {
+                if (set.Contains(item))
+                {
+                    continue;
+                }
+
+                set.Add(item);
+            }
+
+            return set;
+        }
+    }
+}
