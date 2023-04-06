adff790d6eed3a6f1110bbee3ced15ae968030fe
João Neves <joao95neves@gmail.com>
2023-04-06 19:42:47 +0100

    feat(Algorithms): added the optimized solution for two sum

--- CSharp/AlgosAndDataStructs/AlgosAndDataStructs.Algorithms/Arrays/TwoSum.cs
+++ CSharp/AlgosAndDataStructs/AlgosAndDataStructs.Algorithms/Arrays/TwoSum.cs
@@ -17,13 +17,14 @@ namespace AlgosAndDataStructs.Algorithms.Arrays
          * - Output: [0,1]
          * --
          * - Constraints:
-         *   - Theres exactly one solution.
-         *   - Return the index of the numbers that add up to the target.
-         *   - Cannot use the same elem twice.
-         *   - Solution in any order.
+         *       - Theres exactly one solution.
+         *       - Return the index of the numbers that add up to the target.
+         *       - Cannot use the same elem twice.
+         *       - Solution in any order.
          */
         /// <summary>
-        /// O(n^2)
+        /// Time: O(n^2)
+        /// Space: O(1)
         ///
         /// </summary>
         public static IEnumerable<int> BruteForce(IEnumerable<int> nums, int target)
@@ -50,6 +51,62 @@ namespace AlgosAndDataStructs.Algorithms.Arrays
                     }
                 }
             }
+        }
+
+        /**
+         * - We need to make it faster than O(n^2).
+         * - Faster means that we can only pass the input array one time.
+         * - So, we have to keep track of the items already visited.
+         * - This will increase our memory. RAM is more scalable than time ;).
+         * - But we cannot loop that new visit elem list again, because that would still be O(n).
+         * - We have to visit it 1 time (constant time) to get the answer.
+         * - Therefore, we need to:
+         *     - Find the answer for the current number.
+         *     - Check if we have the answer in the answer list.
+         *         - IF NOT: Save that answer in a map (we need a map to return the index of the answer).
+         *         - [get back to first]
+         * - Calculate the answer:
+         *     - Let target = t
+         *     - Let current item = c
+         *     - Let answer = x
+         *         - t = c + x
+         *         - x = ?
+         *         - x = t - c
+         */
+        /// <summary>
+        /// Time: O(n)
+        /// Space: O(n)
+        ///
+        /// </summary>
+        public static IEnumerable<int> Optimized(IEnumerable<int> nums, int target)
+        {
+            if (!IsInputValid(nums))
+            {
+                yield break;
+            }
+
+            var visitedElemMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Count(); ++i)
+            {
+                var answer = CalculateAnswer(target, nums.ElementAt(i));
+
+                if (visitedElemMap.TryGetValue(answer, out int index))
+                {
+                    yield return i;
+                    yield return index;
+                    yield break;
+                }
+                else
+                {
+                    visitedElemMap.Add(nums.ElementAt(i), i);
+                }
+            }
+        }
+
+        private static int CalculateAnswer(int target, int currentNum)
+        {
+            return target - currentNum;
         }
 
         /// <summary>
