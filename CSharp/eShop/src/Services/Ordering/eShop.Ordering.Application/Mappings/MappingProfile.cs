a103c010baecc4904b21a18852b318c0743a2cba
João Neves <joao95neves@gmail.com>
2023-06-07 20:43:31 +0100

    feat(MappingProfile): implement

--- CSharp/eShop/src/Services/Ordering/eShop.Ordering.Application/Mappings/MappingProfile.cs
+++ CSharp/eShop/src/Services/Ordering/eShop.Ordering.Application/Mappings/MappingProfile.cs
@@ -1,9 +1,9 @@
+using eShop.Ordering.Application.Features.Order.Commands.CheckoutOrder;
+using eShop.Ordering.Application.Features.Order.Commands.UpdateOrder;
 using eShop.Ordering.Application.Features.Order.Queries.GetOrdersList;
 using eShop.Ordering.Domain.Entities;
 
 using AutoMapper;
-using eShop.Ordering.Application.Features.Order.Commands.CheckoutOrder;
-using eShop.Ordering.Application.Features.Order.Commands.UpdateOrder;
 
 namespace eShop.Ordering.Application.Mappings
 {
@@ -11,10 +11,57 @@ namespace eShop.Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            // TODO:
             CreateMap<DataOrder, OrderDto>().ReverseMap();
-            CreateMap<DataOrder, CheckoutOrderCommand>().ReverseMap();
-            CreateMap<DataOrder, UpdateOrderCommand>().ReverseMap();
+
+            CreateMap<CheckoutOrderCommand, DataOrder>()
+               .ConvertUsing((comm, _) => new DataOrder()
+               {
+                   UserName = comm.UserName,
+                   TotalPrice = comm.TotalPrice,
+                   BillingInformation = new DataBillingInformation()
+                   {
+                       AddressLine = comm.AddressLine,
+                       Country = comm.Country,
+                       EmailAddress = comm.EmailAddress,
+                       FirstName = comm.FirstName,
+                       LastName = comm.LastName,
+                       State = comm.State,
+                       ZipCode = comm.ZipCode,
+                   },
+                   PaymentInformation = new DataPaymentInformation()
+                   {
+                       CardName = comm.CardName,
+                       CardNumber = comm.CardNumber,
+                       CVV = comm.CVV,
+                       Expiration = comm.Expiration,
+                       PaymentMethod = comm.PaymentMethod,
+                   },
+               });
+
+            CreateMap<UpdateOrderCommand, DataOrder>()
+                .ConvertUsing((comm, _) => new DataOrder()
+                {
+                    UserName = comm.UserName,
+                    TotalPrice = comm.TotalPrice,
+                    BillingInformation = new DataBillingInformation()
+                    {
+                        AddressLine = comm.AddressLine,
+                        Country = comm.Country,
+                        EmailAddress = comm.EmailAddress,
+                        FirstName = comm.FirstName,
+                        LastName = comm.LastName,
+                        State = comm.State,
+                        ZipCode = comm.ZipCode,
+                    },
+                    PaymentInformation = new DataPaymentInformation()
+                    {
+                        CardName = comm.CardName,
+                        CardNumber = comm.CardNumber,
+                        CVV = comm.CVV,
+                        Expiration = comm.Expiration,
+                        PaymentMethod = comm.PaymentMethod,
+                    },
+                });
         }
     }
 }
