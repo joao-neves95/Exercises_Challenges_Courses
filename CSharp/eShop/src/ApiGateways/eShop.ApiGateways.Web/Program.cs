579907d941b58b285c27b0f3501556a3b23f9f98
João Neves <joao95neves@gmail.com>
2023-06-21 20:27:21 +0100

    feat(ApiGateways.Web): added caching

--- CSharp/eShop/src/ApiGateways/eShop.ApiGateways.Web/Program.cs
+++ CSharp/eShop/src/ApiGateways/eShop.ApiGateways.Web/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -11,20 +12,23 @@ namespace eShop.ApiGateways.Web
 
             builder.Configuration.AddJsonFile($"./ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 
+            // Add services to the container.
+
             builder.Logging
                 .AddConfiguration(builder.Configuration.GetSection("Logging"))
                 .AddConsole()
                 .AddDebug()
                 ;
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddOcelot();
+            builder.Services
+                .AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle())
+                ;
 
             var app = builder.Build();
 
